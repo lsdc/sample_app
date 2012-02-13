f36e0f80f55b3f83c95a4c669b89caf533cc6242
LSDC <lestudioducoin@gmail.com>
2012-02-13 09:05:53 +0100

    Debut de l'application example pour apprendre RoR

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c5400ae0a7863a4a73c2273651c88f373a8808ef71e0ae89170f8966fd4a1092dc0021fc61caf39c57fbc957bce01090fabdd8aed827e788493d08ce3f0feacf'
