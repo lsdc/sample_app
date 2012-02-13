2d0fe6f00c309adf8a46b1c24a13bbe1c510bad5
LSDC <lestudioducoin@gmail.com>
2012-02-13 22:24:32 +0100

    debut du chapitre 6

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,14 @@
 module ApplicationHelper
+  #defini un logo si il n'est pas specifié
+  def logo
+    base_logo = image_tag("logo.png", :alt => "Application exemple", :class => "round")
+    if @logo.nil?
+      base_logo
+    else
+      @logo
+    end
+  end
+  
   # verifie que le titre de la pages soit bien defini
   def titre
     base_titre = "Sample App une application qui cartonne"
